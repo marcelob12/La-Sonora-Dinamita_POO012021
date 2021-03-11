e5897678776ee3f4d2cca5bc95eddd810d985666
Marcelo <marcelobran11@gmail.com>
2021-03-10 19:49:19 -0600

    ADD:Ex1

---
+++ introGit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace introGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0 , b=0 ;
+
+            Console.Write("Ingrese un valor para a:");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un valor para b:");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La suma de {a} + {b} es igual a : {addNumbers(a,b)}");
+        }
+
+        public static int addNumbers(int a , int b){
+                return a+b;
+        }
+    }
+}
